cc7291cef9f0f9e96f097f0de07eb91b7252bce3
root <root@DESKTOP-GTOBK44>
2023-10-02 17:34:32 +0100

    add

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * check_cycle - function
+ * @list: list
+ * Return: 0 or 1
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *f, *s;
+
+	if(!list)
+		return(0);
+	f = list;
+	s = list;
+
+	while (list && s && f)
+	{
+		f = f->next;
+		s = s->next->next;
+		if (f == s)
+			return (1);
+	}
+	return (0);
+}
