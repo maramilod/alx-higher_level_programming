7c5d6807f203d522178932680b0309657da75e71
root <root@DESKTOP-GTOBK44>
2023-10-03 15:10:04 +0100

    add

--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
-r = ""
-for i in range(99):
-    r += str(int(i)) + " = " + str(hex(i))
-    if i != 98:
-        r += "\n"
-print(f"{r:s}")
+for number in range(0, 99):
+    print("{} = {}".format(number, hex(number)))
