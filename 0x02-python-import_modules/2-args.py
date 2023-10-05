db8b97fc78037edbd6e5fc7057591595ceaeeb6c
root <root@DESKTOP-GTOBK44>
2023-10-05 13:54:13 +0100

    add

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -4,6 +4,8 @@ if __name__ == "__main__":
     lenght = len(sys.argv)
     if lenght == 1:
         print("0 arguments.")
+    elif lenght == 2:
+        print("1 argument:\n1: {}".format(sys.argv[1]))
     else:
         print("{} arguments:".format(lenght - 1))
         for i in range(1, lenght):
