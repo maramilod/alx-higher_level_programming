7aec31ec9339c5235df0146eaa68d6e175c80ff2
root <root@DESKTOP-GTOBK44>
2023-10-08 14:38:05 +0100

    add

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def add_tuple(tuple_a=(), tuple_b=()):
-    if tuple_a == ():
-        if tuple_b == ():
+    if tuple_a == () or tuple_a is None:
+        if tuple_b == () or tuple_b is None:
             return None
         return tuple_b[0], tuple_b[1]
-    if tuple_b == ():
-        if tuple_a == ():
+    if tuple_b == () or tuple_b is None:
+        if tuple_a == () or tuple_a is None:
             return None
         return tuple_a[0], tuple_a[1]
     b = 0
