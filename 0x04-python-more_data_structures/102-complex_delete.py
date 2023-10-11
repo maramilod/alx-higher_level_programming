a42266abd4ed49e62fd1bff527b568dc42bceae3
root <root@DESKTOP-GTOBK44>
2023-10-11 11:35:14 +0100

    add

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def complex_delete(a_dictionary, value):
+    new = list(a_dictionary.keys())
+    for i in new:
+        if value == a_dictionary.get(i):
+            del a_dictionary[i]
+    return a_dictionary
