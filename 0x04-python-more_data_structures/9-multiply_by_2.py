52ec047989d9ff662dd433174abf95e45d740061
root <root@DESKTOP-GTOBK44>
2023-10-11 11:19:06 +0100

    add

--- 0x04-python-more_data_structures/9-multiply_by_2.py
+++ 0x04-python-more_data_structures/9-multiply_by_2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiply_by_2(a_dictionary):
-    new = a_dictionary.copy
+    new = a_dictionary.copy()
     lis = list(a_dictionary.keys())
     for i in lis:
         new[i] *= 2
