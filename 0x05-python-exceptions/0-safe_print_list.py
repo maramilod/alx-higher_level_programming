15dadf084bd64b39199fcc5855f29fad60d2349f
root <root@DESKTOP-GTOBK44>
2023-10-23 15:36:20 +0100

    add

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
-    j = 0
-    for i in my_list:
-        j += 1
-        if j <= x:
-            print(i ,end="")
-        else:
-            print("\n" ,end="")
-            return j - 1
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+        except IndexError:
+            print("\n", end="")
+            return i
     print("\n" ,end="")
-    return j
+    return i + 1
