e1cbdacc6416615013e7ca8dcb6bbe1464370204
root <root@DESKTOP-GTOBK44>
2023-10-24 19:45:35 +0100

    add

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""med"""
+
+
+class Square:
+    """con"""
+
+    def __init__(self, size=0):
+        """def"""
+
+        if not isinstance(size, int):
+            raise TypeError("")
+        if size < 0:
+            raise ValueError("")
+        self.__size = size
+
+    def area(self):
+        """area of the square"""
+        return self.__size * self.__size
