6a1514f784428134f375392906b05ea7591d4f4d
root <root@DESKTOP-GTOBK44>
2023-10-24 20:06:41 +0100

    add

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -24,7 +24,7 @@ class Square:
             raise ValueError("size must be >= 0")
         self.__size = val
 
-    def erea(self):
+    def area(self):
         """hey again"""
 
         return self.__size ** 2
