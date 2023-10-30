9e8e5e22a754fd53e0eab02e9b6256540671b616
root <root@DESKTOP-GTOBK44>
2023-10-30 19:55:33 +0000

    okok

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -78,7 +78,7 @@ class Rectangle:
         type(self).number_of_instances -= 1
         print("Bye rectangle...")
 
-    @staticmethod    
+    @staticmethod
     def bigger_or_equal(rect_1, rect_2):
         """based on the area"""
         if not isinstance(rect_1, Rectangle):
