ebcbeaf95aa5d4f9c49e103e274e03be879b2c0e
root <root@DESKTOP-GTOBK44>
2023-11-06 19:20:13 +0000

    add

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -6,15 +6,11 @@ y
 """
 
 
-class MyInt(int):
-    """h"""
-    def __new__(cls, *args, **kwargs):
-        """n"""
-        return super(MyInt, cls).__new__(cls, *args, **kwargs)
-
-    def ___eq___(self, other):
-        """ne"""
-        return int(self) != other
+class MyInt (int):
+    """m"""
+    def __eq__(self, other):
+        """nq"""
+        return (int(self) != other)
 
     def __ne__(self, other):
         """eq"""
