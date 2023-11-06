152bfe01deff88387026fb034eab2e0d940b49f9
root <root@DESKTOP-GTOBK44>
2023-11-06 19:35:03 +0000

    add

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -8,6 +8,6 @@ y
 
 def add_attribute(o, a, v):
     """a"""
-    if not hasattr(obj, "__dict__"):
+    if not hasattr(o, "__dict__"):
         raise TypeError("can't add new attribute")
     setattr(o, a, v)
