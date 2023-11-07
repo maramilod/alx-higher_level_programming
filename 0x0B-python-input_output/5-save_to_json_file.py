0356779ace5b347cba211c8a6e6750b3f33e567b
root <root@DESKTOP-GTOBK44>
2023-11-07 18:04:16 +0000

    add

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+h
+e
+y
+"""
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """s"""
+    with open(filename, "w", encoding="utf-8") as m:
+        josn.dump(my_obj, m)
