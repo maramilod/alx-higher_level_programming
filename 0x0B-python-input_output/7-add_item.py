d67f01b4bb8f50f618ceae98cd47f5b066c3a8bf
root <root@DESKTOP-GTOBK44>
2023-11-07 18:49:24 +0000

    add

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,9 +4,9 @@ h
 e
 y
 """
+import sys
 save = __import__("5-save_to_json_file").save_to_json_file
 lo = __import__("6-load_from_json_file").load_from_json_file
-import sys
 
 
 filen = "add_item.json"
