7458a96895759df35c4dd7bfc8d887fd57f138ef
root <root@DESKTOP-GTOBK44>
2023-11-13 10:58:37 +0000

    add

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,21 +6,22 @@ y
 """
 from models.base import Base
 
+
 class Rectangle(Base):
     """class that inherits from Base"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """i"""
+
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
 
-
     @property
     def width(self):
         return self.__width
-
 
     @width.setter
     def width(self, width):
@@ -30,11 +31,9 @@ class Rectangle(Base):
             raise ValueError("width must be > 0")
         self.__width = width
 
-
     @property
     def height(self):
         return self.__height
-
 
     @height.setter
     def height(self, height):
@@ -44,11 +43,9 @@ class Rectangle(Base):
             raise ValueError("height must be > 0")
         self.__height = height
 
-
     @property
     def x(self):
         return self.__x
-
 
     @x.setter
     def x(self, x):
@@ -58,11 +55,9 @@ class Rectangle(Base):
             raise ValueError("x must be >= 0")
         self.__x = x
 
-
     @property
     def y(self):
         return self.__y
-
 
     @y.setter
     def y(self, y):
@@ -72,3 +67,18 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         self.__y = y
 
+    def area(self):
+        """area"""
+        return self.width * self.height
+
+    def display(self):
+        """draw rectangle"""
+        for i in range(self.height):
+            for j in range(self.width):
+                print("#", end="")
+            print()
+
+    def __str__(self):
+        """overriding"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
