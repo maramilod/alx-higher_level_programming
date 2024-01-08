9eed86ddbbc9cf7517c03316f8da8ed7a177caa6
maramilod <marammilod03@gmail.com>
2024-01-08 20:03:57 +0000

    add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,13 +3,13 @@
 const len = process.argv.length;
 if (len > 3) {
   let big = process.argv[2];
-  for (i = 3; i <= len; i++) {
+  for (let i = 2; i <= len; i++) {
     if (process.argv[i] > big) {
       big = process.argv[i];
     }
   }
   let secB = process.argv[2] === big ? process.argv[3] : process.argv[2];
-  for (let i = 2; i < len; i++) {
+  for (let i = 2; i <= len; i++) {
     if (process.argv[i] >= secB && process.argv[i] < big) {
      secB = process.argv[i];
     }
