6384bb652679e759a22367c42332aac1de3d723c
maramilod <marammilod03@gmail.com>
2024-01-08 18:48:52 +0000

    add

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,7 +3,7 @@
 const x = parseInt(process.argv[2]);
 let y = 'X';
 if (x) {
-  for (let i = 0; i < x; i++) {
+  for (let i = 0; i < (x - 1); i++) {
     y = y + 'X';
   }
   for (let i = 0; i < x; i++) {
