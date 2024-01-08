ffa8d258cdb807fa83b5bbeeaf76b734247a92e2
maramilod <marammilod03@gmail.com>
2024-01-08 17:53:03 +0000

    add

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 
 if (process.argv[2]) {
-	if (process.argv[3]) {
-		console.log('Arguments found');
-	} else {
-		console.log('Argument found');
-	}
+		if (process.argv[3]) {
+				console.log('Arguments found');
+		} else {
+				console.log('Argument found');
+		}
 } else {
-	console.log('No argument');
+		console.log('No argument');
 }
