f1d5bec4f64914844e99b08153786bcddb5c6c5e
maramilod <marammilod03@gmail.com>
2024-01-08 20:11:08 +0000

    add

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -11,7 +11,7 @@ if (len > 3) {
   let secB = parseInt(process.argv[2]) === big ? parseInt(process.argv[3]) : parseInt(process.argv[2]);
   for (let i = 2; i <= len; i++) {
     if (process.argv[i] >= secB && parseInt(process.argv[i]) < big) {
-     secB = parseInt(process.argv[i]);
+      secB = parseInt(process.argv[i]);
     }
   }
   console.log(secB);
