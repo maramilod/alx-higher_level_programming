ab99f190659987b61de0a9b013614a3e45703414
maramilod <marammilod03@gmail.com>
2024-01-09 22:10:20 +0000

    add

--- 0x13-javascript_objects_scopes_closures/9-logme.js
+++ 0x13-javascript_objects_scopes_closures/9-logme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
-let count = 0;
-
 exports.logMe = function (item) {
-  count++;
-  console.log(`${count}: ${item}`);
+  if (this.count === undefined) {
+    this.count = 0;
+  } else { this.count++; }
+  console.log(`${this.count}: ${item}`);
 };
