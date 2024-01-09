08d37a01fc44f1fbd30d5bbd676fd159441984a0
maramilod <marammilod03@gmail.com>
2024-01-09 20:55:38 +0000

    add

---
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -0,0 +1,19 @@
+#!/usr/bin/node
+
+module.exports = class Rectangle {
+  constructor (w, h) {
+    if (w && h && w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+  print () {
+    let x = '';
+    for (let i = 0; i < this.width; i++) {
+      x = x + 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+	    console.log(x);
+    }
+  }
+};
