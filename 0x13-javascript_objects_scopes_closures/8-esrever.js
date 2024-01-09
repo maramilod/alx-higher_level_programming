ef5e60c89000ff4080e48b8e84d2cfe7ed76861d
maramilod <marammilod03@gmail.com>
2024-01-09 22:01:41 +0000

    yay

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 
 exports.esrever = function (list) {
-  let len = list.length;
-  let arr = new Array(len);;
+  const len = list.length;
+  const arr = new Array(len);
   for (let i = 0; list[i]; i++) {
     arr[i] = list[len - 1 - i];
   }
