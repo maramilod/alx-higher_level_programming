8057d91edaa08be7f785107cb516146745df3eb0
maramilod <marammilod03@gmail.com>
2024-01-14 18:37:55 +0000

    add

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -13,4 +13,4 @@ if __name__ == "__main__":
     Session.configure(bind=engine)
     session = Session()
     for state in session.query(State).filter(State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))i
+        print("{}: {}".format(state.id, state.name))
