0c70efc627df0562d44bfdf49aafdf1b96cb40cb
maramilod <marammilod03@gmail.com>
2024-01-26 21:10:25 +0000

    add

---
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+import urllib.request
+
+def main():
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as url:
+            headers = dict(url.info())
+            print(headers['X-Request-Id'])
+    except Exception as e:
+        print("An error occurred: {}".format(str(e)))
+
+if __name__ == "__main__":
+    main()
+
